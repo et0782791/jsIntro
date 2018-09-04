a1f34b55d850dfa4494df58cd6b6a45a7c7bab40
Ethan T <ozarktigers88@gmail.com>
2018-09-04 10:42:26 -0500

    init commit

---
+++ conversion.js
@@ -0,0 +1,8 @@
+//Ethan Treece C -> F conversion.
+
+const celsius = 32;
+
+const fahrenheit = (celsius * 9/5) + 32;
+
+console.log(`It is ${celsius} degrees celsius.`);
+console.log(`Which is ${fahrenheit} degrees fahrenheit.`);
