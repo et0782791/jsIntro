69d4650f14fd7466319e5ccbd033f16ef119cb7d
Ethan T <ozarktigers88@gmail.com>
2018-09-04 10:51:50 -0500

    init commit

---
+++ multTable.js
@@ -0,0 +1,8 @@
+// Ethan Treece; Multiplication table. 
+let number = 10;
+for(lcv = 0; lcv < number + 1; lcv++)
+{
+    let result = number*lcv;
+    
+    console.log(`${number} x ${lcv} = ${result}`);
+}
